8ec7d7e48beeb9ff4df812c40699474ac0bb2148
ErikaJPB <erikajpinedab@gmail.com>
2023-02-06 14:25:21 -0500

    firebase

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -2,17 +2,42 @@ import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 interface Props {
-  todo: string;
+  todo: {
+    task: string;
+    id: string;
+    completed: boolean;
+  };
+  toggleComplete: (todo: {
+    task: string;
+    id: string;
+    completed: boolean;
+  }) => void;
+  deleteTodo: (id: string) => void;
 }
 
-function Todo({ todo }: Props) {
+function Todo({ todo, toggleComplete, deleteTodo }: Props) {
   return (
     <li className="flex justify-between bg-slate-200 p-4 my-2 capitalize">
       <div className="flex">
-        <input type="checkbox" />
-        <p className="ml-2 cursor-pointer">{todo}</p>
+        <input
+          onChange={() => toggleComplete(todo)}
+          type="checkbox"
+          checked={todo.completed}
+        />
+        <p
+          className="ml-2 cursor-pointer"
+          onClick={() => toggleComplete(todo)}
+          style={{
+            textDecoration: todo.completed ? "line-through " : "none",
+          }}
+        >
+          {todo.task}
+        </p>
       </div>
-      <button className="flex align-items-center cursor-pointer">
+      <button
+        className="flex align-items-center cursor-pointer"
+        onClick={() => deleteTodo(todo.id)}
+      >
         {<FaRegTrashAlt />}
       </button>
     </li>
